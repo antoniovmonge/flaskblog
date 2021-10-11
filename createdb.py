c1437d839a3a58a92e061d5030bc657fd542a02d
Antonio Vilchez <antoniovmonge@gmail.com>
2021-10-11 12:31:42 +0200

    momentary change to check

--- createdb.py
+++ createdb.py
@@ -11,6 +11,7 @@ load_dotenv()
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 class User(db.Model):
